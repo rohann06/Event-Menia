9fa37e4befa5b619e2479c58fe76647f22d8013b
Rohan Ahire <75326769+rohan-ahire06@users.noreply.github.com>
2024-09-04 01:47:26 +0530

    Nav option added
    -  Added the `Create Event` option
        into the nav bar.
    - Created the new page called
      `createEvent`.

--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -20,6 +20,10 @@ export const menuLinks: HeaderMenuLink[] = [
     href: "/",
   },
   {
+    label: "Create Event",
+    href: "/createEvent",
+  },
+  {
     label: "Debug Contracts",
     href: "/debug",
     icon: <BugAntIcon className="h-4 w-4" />,
